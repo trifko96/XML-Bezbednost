9a9d8e55f4d5aa9fd72e204bf1a79152f5e9523b
vuja96 <45968984+vuja96@users.noreply.github.com>
2019-03-22 20:16:38 +0100

    Odradjene forme za razlicite tipove sertifikata.

--- src/main/resources/static/skripte/admin.js
+++ src/main/resources/static/skripte/admin.js
@@ -227,7 +227,7 @@ $(document).ready(function(){
 					});
 				},
 			});
-		} else if
+		} 
 		
 	});
 	
@@ -235,6 +235,22 @@ $(document).ready(function(){
 		odabraniTip = $("#comboSertifikatN option:selected").val();
 		if(odabraniTip == "ROOT"){
 			dodajZaRoot();
+			dodajDugme();
+		}
+		else if(odabraniTip == "PERSON"){
+			dodajZaOsobu();
+			dodajDugme();
+		}
+		else if(odabraniTip == "APPLICATION"){
+			dodajZaAplikaciju();
+			dodajDugme();
+		}
+		else if(odabraniTip == "ORGANIZATION"){
+			dodajZaOrganizaciju();
+			dodajDugme();
+		}
+		else if(odabraniTip == "EQUIPMENT"){
+			dodajZaOpremu();
 			dodajDugme();
 		}
 	});
@@ -246,6 +262,88 @@ $(document).ready(function(){
 		'<td><input type="date" id="datumIsticanjaRoot"/></td></tr>'+
 		'<tr><td>Naziv organizacije:</td>'+
 		'<td><input type="text" id="nazivOrganizacijeRoot"/></td></tr>';
+		$("#dodajSertifikat").append(pom);
+	}
+	
+	function dodajZaOsobu(){
+		var pom = '<tr><td>Datum izdavanja:</td>'+
+		'<td><input type="date" id="datumIzdavanjaOsoba"/></td></tr>'+
+		'<tr><td>Datum isticanja:</td>'+
+		'<td><input type="date" id="datumIsticanjaOsoba"/></td></tr>'+
+		'<tr><td>Naziv organizacije:</td>'+
+		'<td><input type="text" id="nazivOrganizacijeOsoba"/></td></tr>'+
+		'<tr><td>Nadsertifikat:</td>'+
+		'<td><select id = "comboSertifikat"></select></td></tr>'+
+		'<tr><td>Dozvola za izdavanje:</td>'+
+		'<td><input type="checkbox" id="dozvolaZaIzdavanjeOsoba"/></td></tr>'+
+		'<tr><td>Ime:</td>'+
+		'<td><input type="text" id="imeOsoba"/></td></tr>'+
+		'<tr><td>Prezime:</td>'+
+		'<td><input type="text" id="prezimeOsoba"/></td></tr>'+
+		'<tr><td>Drzava:</td>'+
+		'<td><input type="text" id="drzavaOsoba"/></td></tr>'+
+		'<tr><td>Email:</td>'+
+		'<td><input type="text" id="emailOsoba"/></td></tr>';
+		$("#dodajSertifikat").append(pom);
+	}
+	
+	function dodajZaAplikaciju(){
+		var pom = '<tr><td>Datum izdavanja:</td>'+
+		'<td><input type="date" id="datumIzdavanjaAplikacija"/></td></tr>'+
+		'<tr><td>Datum isticanja:</td>'+
+		'<td><input type="date" id="datumIsticanjaAplikacija"/></td></tr>'+
+		'<tr><td>Naziv organizacije:</td>'+
+		'<td><input type="text" id="nazivOrganizacijeAplikacija"/></td></tr>'+
+		'<tr><td>Nadsertifikat:</td>'+
+		'<td><select id = "comboSertifikat"></select></td></tr>'+
+		'<tr><td>Dozvola za izdavanje:</td>'+
+		'<td><input type="checkbox" id="dozvolaZaIzdavanjeAplikacija"/></td></tr>'+
+		'<tr><td>Naziv aplikacije:</td>'+
+		'<td><input type="text" id="nazivAplikacija"/></td></tr>'+
+		'<tr><td>Verzija:</td>'+
+		'<td><input type="text" id="verzijaAplikacija"/></td></tr>';
+		$("#dodajSertifikat").append(pom);
+	}
+	
+	function dodajZaOrganizaciju(){
+		var pom = '<tr><td>Datum izdavanja:</td>'+
+		'<td><input type="date" id="datumIzdavanjaOrganizacija"/></td></tr>'+
+		'<tr><td>Datum isticanja:</td>'+
+		'<td><input type="date" id="datumIsticanjaOrganizacija"/></td></tr>'+
+		'<tr><td>Naziv organizacije:</td>'+
+		'<td><input type="text" id="nazivOrganizacijeOrganizacija"/></td></tr>'+
+		'<tr><td>Nadsertifikat:</td>'+
+		'<td><select id = "comboSertifikat"></select></td></tr>'+
+		'<tr><td>Dozvola za izdavanje:</td>'+
+		'<td><input type="checkbox" id="dozvolaZaIzdavanjeOrganizacija"/></td></tr>'+
+		'<tr><td>PTT:</td>'+
+		'<td><input type="text" id="pttOrganizacija"/></td></tr>'+
+		'<tr><td>Drzava:</td>'+
+		'<td><input type="text" id="drzavaOrganizacija"/></td></tr>'+
+		'<tr><td>Adresa:</td>'+
+		'<td><input type="text" id="adresaOrganizacija"/></td></tr>';
+		$("#dodajSertifikat").append(pom);
+	}
+	
+	function dodajZaOpremu(){
+		var pom = '<tr><td>Datum izdavanja:</td>'+
+		'<td><input type="date" id="datumIzdavanjaOprema"/></td></tr>'+
+		'<tr><td>Datum isticanja:</td>'+
+		'<td><input type="date" id="datumIsticanjaOprema"/></td></tr>'+
+		'<tr><td>Naziv organizacije:</td>'+
+		'<td><input type="text" id="nazivOrganizacijeOprema"/></td></tr>'+
+		'<tr><td>Nadsertifikat:</td>'+
+		'<td><select id = "comboSertifikat"></select></td></tr>'+
+		'<tr><td>Dozvola za izdavanje:</td>'+
+		'<td><input type="checkbox" id="dozvolaZaIzdavanjeOprema"/></td></tr>'+
+		'<tr><td>MAC:</td>'+
+		'<td><input type="text" id="macOprema"/></td></tr>'+
+		'<tr><td>Naziv opreme:</td>'+
+		'<td><input type="text" id="nazivOprema"/></td></tr>'+
+		'<tr><td>Drzava:</td>'+
+		'<td><input type="text" id="drzavaOprema"/></td></tr>'+
+		'<tr><td>ID opreme:</td>'+
+		'<td><input type="text" id="idOprema"/></td></tr>';
 		$("#dodajSertifikat").append(pom);
 	}
 	
