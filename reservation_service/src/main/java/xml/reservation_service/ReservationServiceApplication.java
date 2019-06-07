dabc3368e4b14c41cc54cc0989039660e1d231c4
vuja96 <45968984+vuja96@users.noreply.github.com>
2019-06-07 13:26:34 +0200

    Dodate aplikacije

---
+++ reservation_service/src/main/java/xml/reservation_service/ReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package xml.reservation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
+
+}
