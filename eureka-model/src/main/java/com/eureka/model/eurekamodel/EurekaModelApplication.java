36495f21d80bdedb0673d7006ce7a5d8f21f0166
trifko96 <trifko96@gmail.com>
2019-06-07 12:35:10 +0200

    new projects

---
+++ eureka-model/src/main/java/com/eureka/model/eurekamodel/EurekaModelApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.model.eurekamodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaModelApplication.class, args);
+	}
+
+}
