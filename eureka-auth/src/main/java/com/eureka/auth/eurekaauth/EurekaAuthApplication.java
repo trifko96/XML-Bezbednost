daa4668f957583811a4668e287d2a6a85867af70
trifko96 <trifko96@gmail.com>
2019-06-07 12:31:42 +0200

    new project

---
+++ eureka-auth/src/main/java/com/eureka/auth/eurekaauth/EurekaAuthApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.auth.eurekaauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaAuthApplication.class, args);
+	}
+
+}
