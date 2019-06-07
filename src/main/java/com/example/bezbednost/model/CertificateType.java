aaba78563e9799bcf25cb3dc9f989089ae6b6459
trifko96 <trifko96@gmail.com>
2019-06-07 08:33:57 +0200

    remove

--- src/main/java/com/example/bezbednost/model/CertificateType.java
+++
@@ -1,9 +0,0 @@
-package com.example.bezbednost.model;
-
-public enum CertificateType {
-	ROOT,
-	PERSON,
-	APPLICATION,
-	ORGANIZATION,
-	EQUIPMENT,
-}
--- src/main/resources/static/skripte/pom2.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-
-$("small").each(function(index, el){
-		$(this).hide();
-	});
-
-});
