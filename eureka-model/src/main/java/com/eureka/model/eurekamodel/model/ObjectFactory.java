55484b9e067cbfa5f246b765f0df8cc19a9b5b20
vuja96 <45968984+vuja96@users.noreply.github.com>
2019-06-19 08:49:06 +0200

    Dorada osnove za sinhronizaciju.

--- eureka-model/src/main/java/com/eureka/model/eurekamodel/model/ObjectFactory.java
+++ eureka-model/src/main/java/com/eureka/model/eurekamodel/model/ObjectFactory.java
@@ -41,4 +41,88 @@ public class ObjectFactory {
 	public Reservation createReservation() {
 		return new Reservation();
 	}
+	
+	public User createUser() {
+		return new User();
+	}
+	
+	public AddAccommodationRequest createAddAccommodationRequest() {
+		return new AddAccommodationRequest();
+	}
+	
+	public AddAccommodationResponse createAddAccommodationResponse() {
+		return new AddAccommodationResponse();
+	}
+	
+	public ApproveReservationRequest createApproveReservationRequest() {
+		return new ApproveReservationRequest();
+	}
+		
+	public ApproveReservationResponse createApproveReservationResponse() {
+		return new ApproveReservationResponse();
+	}
+	
+	public GetAccommodationServiceRequest createAccommodationServiceRequest() {
+		return new GetAccommodationServiceRequest();
+	}
+	
+	public GetAccommodationServiceResponse createAccommodationServiceResponse() {
+		return new GetAccommodationServiceResponse();
+	}
+	
+	public GetAccommodationTypeRequest createAccommodationTypeRequest() {
+		return new GetAccommodationTypeRequest();
+	}
+	
+	public GetAccommodationTypeResponse createGetAccommodationTypeResponse() {
+		return new GetAccommodationTypeResponse();
+	}
+	
+	public GetAgentRequest createAgentRequest() {
+		return new GetAgentRequest();
+	}
+	
+	public GetAgentResponse createAgentResponse() {
+		return new GetAgentResponse();
+	}
+	
+	public GetMessaggeRequest createGetMessaggeRequest() {
+		return new GetMessaggeRequest();
+	}
+	
+	public GetMessaggeResponse createGetMessaggeResponse() {
+		return new GetMessaggeResponse();
+	}
+	
+	public GetRecensionRequest createGetRecensionRequest() {
+		return new GetRecensionRequest();
+	}
+	
+	public GetRecensionResponse createGetRecensionResponse() {
+		return new GetRecensionResponse();
+	}
+	
+	public GetReservationRequest createGetReservationRequest() {
+		return new GetReservationRequest();
+	}
+	
+	public GetReservationResponse createGetReservationResponse() {
+		return new GetReservationResponse();
+	}
+	
+	public MessaggeRequest createMessaggeRequest() {
+		return new MessaggeRequest();
+	}
+	
+	public MessaggeResponse createMessaggeResponse() {
+		return new MessaggeResponse();
+	}
+	
+	public ReservationRequest createReservationRequest() {
+		return new ReservationRequest();
+	}
+	
+	public ReservationResponse createReservationResponse() {
+		return new ReservationResponse();
+	}
 }
