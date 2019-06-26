d582cd266409c75500266c80ef1a87b8f0c11636
trifko96 <trifko96@gmail.com>
2019-06-26 13:33:43 +0200

    security config

---
+++ reservation_service/src/main/java/xml/reservation_service/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,27 @@
+package xml.reservation_service.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+	
+	@Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "UNAUTHORIZED");
+        
+        // How to customize the retured message?
+        // Link: https://stackoverflow.com/a/40791087
+       // System.out.println("AAAAAAAAAAAAAAAAAAA" + httpServletRequest.getServletPath());
+        String json = String.format("{\"message\": \"%s\"}", e.getStackTrace().toString());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);       
+    }
+}
---
+++ reservation_service/src/main/java/xml/reservation_service/security/SecurityTokenConfig.java
@@ -0,0 +1,47 @@
+package xml.reservation_service.security;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.eureka.model.eurekamodel.security.JwtConfig;
+
+@EnableWebSecurity 	// Enable security config. This annotation denotes config for spring security.
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private JwtConfig jwtConfig;
+ 
+	@Override
+  	protected void configure(HttpSecurity http) throws Exception {
+    	   http
+		.csrf().disable()
+		    // make sure we use stateless session; session won't be used to store user's state.
+	 	    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) 	
+		.and()
+		    // handle an authorized attempts 
+		    .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)) 	
+		.and()
+		   // Add a filter to validate the tokens with every request
+		   .addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
+		// authorization requests config
+		.authorizeRequests()
+		.antMatchers("/user/**").hasRole("USER")
+		.antMatchers("/all/**").permitAll()
+		.antMatchers("/soap/**").hasRole("AGENT")
+		.anyRequest().authenticated(); 
+	}
+	
+	@Bean
+  	public JwtConfig jwtConfig() {
+    	   return new JwtConfig();
+  	}
+}
