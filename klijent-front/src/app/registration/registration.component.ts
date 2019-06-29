ddb744684dba29c1abd043a9d80189d79d590099
vuja96 <45968984+vuja96@users.noreply.github.com>
2019-06-29 19:40:45 +0200

    Uradjena validacija za registraciju.

--- klijent-front/src/app/registration/registration.component.ts
+++ klijent-front/src/app/registration/registration.component.ts
@@ -3,6 +3,7 @@ import { UseExistingWebDriver } from 'protractor/built/driverProviders';
 import { User } from '../model/User';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-registration',
@@ -13,6 +14,7 @@ export class RegistrationComponent implements OnInit {
 
   user : User = new User();
   messagge : String;
+  
   constructor(private http : HttpClient, private router : Router) { }
 
   
@@ -20,6 +22,44 @@ export class RegistrationComponent implements OnInit {
   }
 
   submit(){
+    let check : boolean = false;
+
+    if(this.user.name == ""){
+      $("#name").addClass('border-danger');
+      check = true;
+    } else {
+      $("#name").removeClass('border-danger');
+    }
+
+    if(this.user.surname == ""){
+      $("#surname").addClass('border-danger');
+      check = true;
+    } else {
+      $("#surname").removeClass('border-danger');
+    }
+
+    if(this.user.username == ""){
+      $("#username").addClass('border-danger');
+      check = true;
+    } else {
+      $("#username").removeClass('border-danger');
+    }
+
+    if(this.user.email == ""){
+      $("#email").addClass('border-danger');
+      check = true;
+    } else {
+      $("#email").removeClass('border-danger');
+    }
+
+    if(this.user.password == ""){
+      $("#password").addClass('border-danger');
+      check = true;
+    } else {
+      $("#password").removeClass('border-danger');
+    }
+
+    if(!check){
     this.http.post<User>("api/auth/User/registration", this.user).subscribe(
       data=>{
         this.router.navigate(["/login"]);
@@ -31,6 +71,8 @@ export class RegistrationComponent implements OnInit {
       
 
     );
+
+    }
   }
 
 }
